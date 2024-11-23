8e57c69df6c7b6c76fadbd88783d95366c932561
leoyliugit config --global user.email liuyuanyi1208@gmail.com <ziyuan.shen@mail.utoronto.ca>
2024-11-23 17:34:04 -0500

    update 02-download_data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,22 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data for analysis from Wordbank database of children’s vocabulary growth
+# Author: Ziyuan Shen
+# Date: 23 November 2024 
+# Contact: ziyuan.shen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: The `tidyverse` package must be installed
+# Any other information needed? Make sure you are in the `Factors-Influencing-Comprehension-in-Early-Childhood` rproj
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+raw_data <- read_csv(
+  file = "data/01-raw_data/raw_data.csv",
+  show_col_types = TRUE
+)
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+write_csv(raw_data, "data/01-raw_data/raw_data.csv")
