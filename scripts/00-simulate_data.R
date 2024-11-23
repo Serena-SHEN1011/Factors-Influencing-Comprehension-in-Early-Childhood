9551f070e0e819fdaef55aaa33121cb497c26eaf
leoyliugit config --global user.email liuyuanyi1208@gmail.com <ziyuan.shen@mail.utoronto.ca>
2024-11-23 17:51:11 -0500

    simulate data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,12 +1,12 @@
 #### Preamble ####
 # Purpose: Simulates a dataset of Australian electoral divisions, including the 
   #state and party that won each division.
-# Author: Rohan Alexander
-# Date: 26 September 2024
+# Author: Ziyuan Shen
+# Date: 23 November 2024 
 # Contact: rohan.alexander@utoronto.ca
 # License: MIT
 # Pre-requisites: The `tidyverse` package must be installed
-# Any other information needed? Make sure you are in the `starter_folder` rproj
+# Any other information needed? Make sure you are in the `Factors-Influencing-Comprehension-in-Early-Childhood` rproj
 
 
 #### Workspace setup ####
@@ -15,38 +15,32 @@ set.seed(853)
 
 
 #### Simulate data ####
-# State names
-states <- c(
-  "New South Wales",
-  "Victoria",
-  "Queensland",
-  "South Australia",
-  "Western Australia",
-  "Tasmania",
-  "Northern Territory",
-  "Australian Capital Territory"
+# Define the number of observations
+num_obs <- 1000
+
+# Define possible values for each variable
+age_in_months <- unique(c(18, 24, 30, 36))
+comprehension <- sample(100:300, num_obs, replace = TRUE)
+production <- sample(10:150, num_obs, replace = TRUE)
+is_norming <- c(0, 1)
+birth_orders <- 1:4
+caregiver_educations <- c("Graduate", "College", "Some College", "High School")
+races <- c("White", "Black", "Asian", "Other")
+sex_values <- c(0, 1) # 0 for female, 1 for male
+monolingual_values <- c(0, 1)
+
+# Generate the dataset
+simulated_data <- tibble(
+  age = sample(age_in_months, num_obs, replace = TRUE),
+  comprehension = comprehension,
+  production = production,
+  is_norming = sample(is_norming, num_obs, replace = TRUE),
+  birth_order = sample(birth_orders, num_obs, replace = TRUE),
+  caregiver_education = sample(caregiver_educations, num_obs, replace = TRUE),
+  race = sample(races, num_obs, replace = TRUE),
+  sex = sample(sex_values, num_obs, replace = TRUE),
+  monolingual = sample(monolingual_values, num_obs, replace = TRUE)
 )
-
-# Political parties
-parties <- c("Labor", "Liberal", "Greens", "National", "Other")
-
-# Create a dataset by randomly assigning states and parties to divisions
-analysis_data <- tibble(
-  division = paste("Division", 1:151),  # Add "Division" to make it a character
-  state = sample(
-    states,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.25, 0.25, 0.15, 0.1, 0.1, 0.1, 0.025, 0.025) # Rough state population distribution
-  ),
-  party = sample(
-    parties,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.40, 0.40, 0.05, 0.1, 0.05) # Rough party distribution
-  )
-)
-
 
 #### Save data ####
-write_csv(analysis_data, "data/00-simulated_data/simulated_data.csv")
+write_csv(simulated_data, "data/00-simulated_data/simulated_data.csv")
